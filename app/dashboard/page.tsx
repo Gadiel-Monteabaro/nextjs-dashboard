f17066f937112cd9c1844996fccf5b31b4630f01
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-11-19 15:30:26 -0300

    cards skeleton implemented

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
-  CardSkeleton,
+  CardsSkeleton,
 } from "@/app/ui/skeletons";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchCardData } from "@/app/lib/data";
@@ -17,7 +17,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>

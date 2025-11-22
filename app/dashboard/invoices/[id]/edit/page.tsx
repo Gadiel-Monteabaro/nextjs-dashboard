31732697b286d3db344aa2ab049a7b9ffd2b48aa
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-11-22 02:46:52 -0300

    feat: error, and page not found 404

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -11,7 +12,9 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchCustomers(),
   ]);
 
-  console.log(invoice);
+  if (!invoice) {
+    return notFound();
+  }
 
   return (
     <main>
